085c510a5b2461c65d1da0175ec6d704078dfb19
Ethan <eribe403@gmail.com>
2018-02-14 23:48:49 -0800

    2

--- user_sys/accounts/forms.py
+++ user_sys/accounts/forms.py
@@ -4,22 +4,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class RegistrationForm(UserCreationForm):
     # line only necessary if field is REQUIRED, add more if want to make first/last name req
-    email = forms.EmailField(required=True)
+    # email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = (
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            # 'password1',
-            # 'password2'
-        )
+        fields = ('username', 'email',)
     def save(self, commit=True):
         user= super(RegistrationForm, self).save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
 
         if commit:
--- user_sys/accounts/views.py
+++ user_sys/accounts/views.py
@@ -6,6 +6,7 @@ from accounts.forms import (
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
 from django.contrib.auth import update_session_auth_hash, authenticate, login
+from django.contrib import messages
 # from django.contrib.auth.decorators import login_required
 
 
@@ -15,11 +16,18 @@ def index(request):
 def register(request):
   if request.method=='POST':
     form = RegistrationForm(request.POST)
+    print(request.POST)
     if form.is_valid():
       form.save()
-      new_user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password1'])
+      new_user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
       login(request, new_user)
       return redirect('accounts:view_profile')
+    else:
+      # clean the data (?)
+      username = form['username'].value()
+      if User.objects.filter(username=username).exists():
+        messages.add_message(request, messages.ERROR, 'username exists')
+        return render(request, 'accounts/reg_form.html')
   else:
     form = RegistrationForm()
     args = {'form': form}
@@ -33,21 +41,35 @@ def view_profile(request):
 
 def edit_profile(request):
   if request.method == 'POST':
-    form = EditProfileForm(data=request.POST, instance=request.user)
+    if 'old_password' in request.POST:
+      form = PasswordChangeForm(data=request.POST, user=request.user)
+      if form.is_valid():
+        form.save()
+        update_session_auth_hash(request, form.user)
+        # success msg
+        return redirect('accounts:view_profile')
+      else:
+        # add error
+        return redirect('accounts:edit_profile')
 
-    if form.is_valid():
-      form.save()
-      return redirect('accounts:view_profile')
+    else:  
+      form = EditProfileForm(data=request.POST, instance=request.user)
+      if form.is_valid():
+        form.save()
+        #success msg
+        return redirect('accounts:view_profile')
+      else:
+        # add error
+        return redirect('accounts:edit_profile')
+
   else:
-    form = EditProfileForm(instance=request.user)
-    args = {'form':form}
-    return render(request, 'accounts/edit_profile.html', args)
-
+    return render(request, 'accounts/edit_profile.html')
 
 def change_password(request):
   if request.method == 'POST':
     form = PasswordChangeForm(data=request.POST, user=request.user)
-
+    print(request.POST)
+    print('old_password' in request.POST)
     if form.is_valid():
       form.save()
       # to keep user logged in after redirect
@@ -56,6 +78,4 @@ def change_password(request):
     else:
       return redirect('accounts:change-password')
   else:
-    form = PasswordChangeForm(user=request.user)
-    args = {'form':form}
-    return render(request, 'accounts/change_password.html', args)
+    return render(request, 'accounts/change_password.html')
